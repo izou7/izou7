bcdcf660d025c16cae76acd0d896e6017cf37c52
zhangxiaoyinghao <zhangxiaoyinghao@sina.com>
2015-02-12 13:51:16 +0800

    commit Activity.java

--- src/main/java/cn/chinattclub/izou7/entity/Activity.java
+++ src/main/java/cn/chinattclub/izou7/entity/Activity.java
@@ -1,10 +1,8 @@
 package cn.chinattclub.izou7.entity;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -13,11 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
-import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.NotFound;
 import org.hibernate.annotations.NotFoundAction;
 
